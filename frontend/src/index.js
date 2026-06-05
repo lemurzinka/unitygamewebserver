18c7f729ac1d0b29be36f6d9aec783966f6acdfe
Denis Gevkach <denisgevkach@gmail.com>
2026-06-05 13:31:27 +0300

    Fix Google OAuth init: remove StrictMode to prevent multiple initializations

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,11 +7,9 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>
 );
 
 reportWebVitals();
